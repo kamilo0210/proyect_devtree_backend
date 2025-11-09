937b0d4ac2ace86790edfb9c75a6161790c107e7
Camilo Pinzón <115189338+kamilo0210@users.noreply.github.com>
2025-11-09 01:55:18 +0000

    feat: Adding some handlers

---
+++ src/handlers/index.ts
@@ -0,0 +1,62 @@
+import User from "../models/User"
+import {Request} from 'express'
+import { checkPassword, hashPassword } from "../utils/auths"
+import slug from 'slug'
+import { generateJWT } from "../utils/jwt"
+
+
+export const createAccount = async (req:Request, res:any) =>{
+   
+
+    const {email, password} = req.body
+    const userExists = await User.findOne({email})
+    
+    if(userExists){
+     const error = new Error("Un usuario con ese email ya esta registrado")
+      return res.status(409).json({error: error.message})
+    }
+    
+    const handle = slug(req.body.handle, '')
+    const handleExists = await User.findOne({handle})
+    if(handleExists){
+     const error = new Error("Nombre de usuario no disponible")
+      return res.status(400).json({error: error.message})
+    }
+
+    const user = new User(req.body)
+    user.handle = handle
+    user.password = await hashPassword(password)
+
+    await user.save()
+
+    res.status(201).send('Registro creado correctamente')
+
+}
+
+export const login = async (req:Request, res:any) => {
+    
+    const {email, password} = req.body
+    const user = await User.findOne({email})
+    
+    //Revisar si el usuario esta registrado
+    if(!user){
+        const error = new Error("El usuario ingresado no esta registrado")
+         return res.status(404).json({error:error.message})
+     }
+
+    //Comprobar el password
+    const isPasswordCorrect = await checkPassword(password, user.password)
+    if(!isPasswordCorrect){
+        const error = new Error("Contraseña incorrecta")
+        return res.status(401).json({error:error.message})
+    }
+    const token = generateJWT({id:user._id})
+    res.status(200).send(token)
+    
+
+}
+
+
+export const getUser = async  (req:Request, res:any) => {
+    res.json(req.user)
+}
---
+++ src/middleware/auth.ts
@@ -0,0 +1,43 @@
+import { Request, NextFunction } from "express"
+import User from "../models/User"
+import jwt from "jsonwebtoken"
+import { IUser } from "../interfaces/User.interface"
+
+declare global {
+    namespace Express{
+        interface Request{
+            user?: IUser
+        }
+    }
+}
+
+export const authtenticate = async(req:Request, res, next:NextFunction) =>{
+    const bearer = req.headers.authorization
+
+    if(!bearer) {
+        const error = new Error ("No autorizado")
+        return res.status(401).json({error:error.message})
+    }
+
+    const [, token] = bearer.split(' ')
+
+    if(!token) {
+        const error = new Error ("No autorizado")
+        return res.status(401).json({error:error.message})
+    }
+
+    try {
+        const result = jwt.verify(token, process.env.JWT_SECRET)
+        if(typeof result === 'object' && result.id) {
+            const user = await User.findById(result.id).select('-password')
+            if(!user){
+                const error = new Error ("El usuario no existe")
+                return res.status(404).json({error:error.message})
+            }
+            req.user = user
+            next()
+        }
+    } catch (error) {
+        res.status(500).json({error:"Token no valido"})
+    }
+}
